b0b6fc323887ff029637c4d39187d1b009fd15d4
AlvaroCerpa <alvaro.baron1959@gmail.com>
2026-01-15 13:33:52 -0500

    successful project 1

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-const express = require('express');
+const express = require("express");
+
+const mongodb = require("./db/contacts.js");
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'));
+app.use("/", require("./routes/index.js"));
 
-app.listen(port, ()=>{console.log(`Running on port ${port}`)})
+mongodb.initDb((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    app.listen(port, () => {
+      console.log(`Database is listening and node running on port ${port}`);
+    });
+  }
+});
