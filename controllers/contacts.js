a9010a795e3c9d6e7dd90a280cfc122ceffb6841
AlvaroCerpa <alvaro.baron1959@gmail.com>
2026-02-19 13:03:12 -0500

    done

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,7 +1,8 @@
 import Contact from "../models/contacts.js";
+import env from "dotenv";
+env.config();
 
-const apiKey =
-  "Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68Xwaj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N";
+const apiKey = env.API_KEY;
 
 const createContact = async (req, res) => {
   // Validate request
